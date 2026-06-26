fe6d8feeb406e7fefb0be2db9953625fde02685b
Leonard Lomude <leonardlomude@icloud.com>
2026-06-26 21:30:26 +0300

    ?

--- lib/auth.ts
+++ lib/auth.ts
@@ -3,8 +3,7 @@ import { dash } from "@better-auth/infra";
 
 export const auth = betterAuth({
   baseURL: "http://localhost:3000/",
-  plugins: [
-    dash(),
-  ],
+  secret: process.env.BETTER_AUTH_SECRET,
+  plugins: [dash()],
 });
 
